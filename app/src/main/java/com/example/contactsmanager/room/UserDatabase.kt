7cd2c08b68de375fdc3a0a3b47ba4460a7e243b1
Evgeny <evgeny.hyvarinen@tietoevry.com>
2023-04-21 22:20:53 +0300

    UserDatabase singleton

---
+++ app/src/main/java/com/example/contactsmanager/room/UserDatabase.kt
@@ -0,0 +1,31 @@
+package com.example.contactsmanager.room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [User::class], version = 1)
+abstract class UserDatabase : RoomDatabase() {
+    abstract val userDAO: UserDAO
+
+    // singleton
+    companion object {
+        @Volatile // make field visible for threads
+        private var INSTANCE: UserDatabase ?= null
+        fun getInstance(context: Context): UserDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    // creating db object
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        UserDatabase::class.java,
+                        "users_db"
+                    ).build()
+                }
+                return instance
+            }
+        }
+    }
+}
