200b90cd2bf1dc00a8a5d9939f927e330c529731
Evgeny <evgeny.hyvarinen@tietoevry.com>
2023-04-21 21:52:58 +0300

    Data entity for DB, fixes

--- app/src/main/java/com/example/contactsmanager/room/User.kt
+++ app/src/main/java/com/example/contactsmanager/room/User.kt
@@ -2,9 +2,11 @@ package com.example.contactsmanager.room
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "user")
 data class User (
+    @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "user_id")
     val id: Int,
     @ColumnInfo(name = "user_name")
